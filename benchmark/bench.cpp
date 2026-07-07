d434ad5dfee9c1e60cbca6dd09bdb61e68aaa8d2
Hugo L <lefevrehugo@ymail.com>
2026-07-08 00:01:35 +0900

    feat(benchmark): full contact computation using JVRC1 foot
    - Uses contactPolytopeJob instead of full DynamicPolytope class (avoid real threads)
    - Default job inputs using same structure as in normal usage

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -1,7 +1,6 @@
 /* @todo
-- regarder instanciation faux robot comme dans tests unitaires mc_rtc
-- vérifier à quel point nb total de dofs impacte (taille calcul jacobienne inertie etc)
-- déterminer runtime loop quand le future() du thread de la lib est récupéré
+- Try out fake robots instanciations with chosen number of dofs like in mc_rtc unit tests
+- Check if total number of dofs in the robot has an impact (size of jacobian and inertia matrices)
 */
 
 #include <mc_rbdyn/RobotLoader.h>
@@ -9,7 +8,7 @@
 #include <benchmark/benchmark.h>
 #include <mc_dynamic_polytopes/DynamicPolytope.h>
 
-static void BM_PolytopeJVRC1(benchmark::State & state)
+static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
 {
   // Setup part (not timed)
   auto robots = mc_rbdyn::loadRobot(*mc_rbdyn::RobotLoader::get_robot_module("JVRC1"));
@@ -20,29 +19,46 @@ static void BM_PolytopeJVRC1(benchmark::State & state)
   mc_rbdyn::RobotsPtr robotsList(mc_rbdyn::Robots::make());
   robotsList->robotCopy(robot, robot.name());
   robotsList->robotCopy(ground, ground.name());
-  // robotsList->load()
 
   mc_rtc::Configuration config;
   // Add required configuration keys if needed
   config.add("possibleContacts", std::vector<std::string>{"LeftFoot", "RightFoot"});
   config.add("withMoments", false);
-  config.add("computeRegions", true);
+  config.add("computeRegions", false);
   config.add("HrepMode", false);
   config.add("withFriction", true);
-  mc_dynamic_polytopes::DynamicPolytope polytopeComputer("test", robot, config);
 
-  // Set current active contacts (normally in controller run)
-  std::map<std::string, mc_rbdyn::Contact &> Contacts; // const_cast<mc_rbdyn::Robots &>(*robotsList)
+  // Create simple contact
   mc_rbdyn::Contact leftFootContact(const_cast<mc_rbdyn::Robots &>(*robotsList.get()), 0, 1, "LeftFoot", "AllGround");
-  Contacts.emplace(leftFootContact.r1Surface()->name(), leftFootContact);
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
 
   // Testing part
   for(auto _ : state)
   {
-    polytopeComputer.setControllerContacts(Contacts);
-    polytopeComputer.computeRegions();
-    // @todo wait for polytope result
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &leftFootContact;
+    contactJob.ctl_ = NULL;
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "LeftFoot";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("LeftFoot").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_PolytopeJVRC1);
+BENCHMARK(BM_PolytopeJVRC1_3D_6DOF)->Unit(benchmark::kMillisecond);
