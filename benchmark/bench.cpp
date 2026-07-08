35a427f49d6ca990f69583d27fc5841da1f3edff
Hugo L <lefevrehugo@ymail.com>
2026-07-09 00:31:50 +0900

    feat(benchmark): add dummy surface to choose number of dofs
    Create a dummy square surface on other bodies of the JVRC1 to vary kinematic chain length

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -8,6 +8,15 @@
 #include <benchmark/benchmark.h>
 #include <mc_dynamic_polytopes/DynamicPolytope.h>
 
+void addDummySurface(mc_rbdyn::Robot & robot, std::string surfaceName, std::string parentBodyName)
+{
+  // create a dummy square surface
+  std::vector<std::pair<double, double>> points = {{-0.1, -0.1}, {0.1, -0.1}, {0.1, 0.1}, {-0.1, 0.1}};
+  std::shared_ptr<mc_rbdyn::Surface> surfacePtr(
+      new mc_rbdyn::PlanarSurface(surfaceName, parentBodyName, sva::PTransformd::Identity(), "plastic", points));
+  robot.addSurface(surfacePtr);
+}
+
 static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
 {
   // Setup part (not timed)
@@ -58,7 +67,9 @@ static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_PolytopeJVRC1_3D_6DOF)->Unit(benchmark::kMillisecond)->Repetitions(100)->DisplayAggregatesOnly(true);
+BENCHMARK(BM_PolytopeJVRC1_3D_6DOF)->Unit(benchmark::kMillisecond);
+// @todo consider setting number of repetitons to get standard deviation with
+// ->Repetitions(30)->DisplayAggregatesOnly(true);
 
 static void BM_PolytopeJVRC1_6D_6DOF(benchmark::State & state)
 {
@@ -110,4 +121,324 @@ static void BM_PolytopeJVRC1_6D_6DOF(benchmark::State & state)
   }
 }
 
-BENCHMARK(BM_PolytopeJVRC1_6D_6DOF)->Unit(benchmark::kMillisecond)->Repetitions(100)->DisplayAggregatesOnly(true);
+BENCHMARK(BM_PolytopeJVRC1_6D_6DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_3D_7DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", false);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P
+  // ==> contact on R_ELBOW_P link is 7 dofs
+  addDummySurface(robot, "RForearm", "R_ELBOW_P_S");
+  mc_rbdyn::Contact RForearmContact(*robots, 0, 1, "RForearm", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RForearmContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RForearm";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RForearm").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_3D_7DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_6D_7DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", true);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P
+  // ==> contact on R_ELBOW_P link is 7 dofs
+  addDummySurface(robot, "RForearm", "R_ELBOW_P_S");
+  mc_rbdyn::Contact RForearmContact(*robots, 0, 1, "RForearm", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RForearmContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RForearm";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RForearm").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_6D_7DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_3D_8DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", false);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P -> R_ELBOW_Y
+  // ==> contact on R_ELBOW_Y link is 8 dofs
+  addDummySurface(robot, "RWrist", "R_ELBOW_Y_S");
+  mc_rbdyn::Contact RWristContact(*robots, 0, 1, "RWrist", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RWristContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RWrist";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RWrist").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_3D_8DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_6D_8DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", true);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P -> R_ELBOW_Y
+  // ==> contact on R_ELBOW_Y link is 8 dofs
+  addDummySurface(robot, "RWrist", "R_ELBOW_Y_S");
+  mc_rbdyn::Contact RWristContact(*robots, 0, 1, "RWrist", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RWristContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RWrist";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RWrist").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_6D_8DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_3D_9DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", false);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P -> R_ELBOW_Y
+  // -> R_WRIST_R
+  // ==> contact on R_WRIST_R_S link is 9 dofs
+  addDummySurface(robot, "RHand", "R_WRIST_R_S");
+  mc_rbdyn::Contact RHandContact(*robots, 0, 1, "RHand", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RHandContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RHand";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RHand").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_3D_9DOF)->Unit(benchmark::kMillisecond);
+
+static void BM_PolytopeJVRC1_6D_9DOF(benchmark::State & state)
+{
+  // Setup part (not timed)
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
+
+  mc_rtc::Configuration config;
+  config.add("withMoments", true);
+  config.add("HrepMode", false);
+  config.add("withFriction", true);
+
+  // Create simple contact
+  // JVRC1 arm: WAIST_Y -> WAIST_P -> WAIST_R -> R_SHOULDER_P -> R_SHOULDER_R -> R_SHOULDER_Y -> R_ELBOW_P -> R_ELBOW_Y
+  // -> R_WRIST_R
+  // ==> contact on R_WRIST_R_S link is 9 dofs
+  addDummySurface(robot, "RHand", "R_WRIST_R_S");
+  mc_rbdyn::Contact RHandContact(*robots, 0, 1, "RHand", "AllGround");
+
+  // polytope dimension depending on moments option
+  config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
+
+  // Testing part
+  for(auto _ : state)
+  {
+    // Intentionally building job and input in the test loop to emulate lib runtime
+    mc_dynamic_polytopes::ContactPolytopeJob contactJob;
+    contactJob.contactRBDyn_ = &RHandContact;
+    contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
+
+    // "job" inputs (simple defaults)
+    auto & input = contactJob.input();
+    input.contactName = "RHand";
+    input.mb = robot.mb();
+    input.mbc = robot.mbc();
+    input.tl = robot.tl();
+    input.tu = robot.tu();
+    input.surface = robot.surface("RHand").copy();
+    input.surfacePose = input.surface->X_0_s(robot);
+    input.accW = robot.accW();
+    input.refContactTransform = sva::PTransformd::Identity();
+    input.frictionCoefficient = 0.7;
+    input.forceScalingFactor = 1.0;
+
+    // run computation manually
+    contactJob.computeJob();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_PolytopeJVRC1_6D_9DOF)->Unit(benchmark::kMillisecond);
