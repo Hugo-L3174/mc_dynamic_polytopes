1df4ab53db5bfcab48a6b7cf335a29720bd0b027
Hugo L <lefevrehugo@ymail.com>
2026-07-08 23:09:12 +0900

    feat(benchmark): simplify loading and config

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -1,6 +1,6 @@
 /* @todo
-- Try out fake robots instanciations with chosen number of dofs like in mc_rtc unit tests
 - Check if total number of dofs in the robot has an impact (size of jacobian and inertia matrices)
+- Check if need to set random posture and dynamics to robot ? some calculations may be optimized if full zeros
 */
 
 #include <mc_rbdyn/RobotLoader.h>
@@ -11,25 +11,20 @@
 static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
 {
   // Setup part (not timed)
-  auto robots = mc_rbdyn::loadRobot(*mc_rbdyn::RobotLoader::get_robot_module("JVRC1"));
-  auto & robot = robots->robot();
-  auto envs = mc_rbdyn::loadRobot(*mc_rbdyn::RobotLoader::get_robot_module("env/ground"));
-  auto & ground = envs->robot();
-
-  mc_rbdyn::RobotsPtr robotsList(mc_rbdyn::Robots::make());
-  robotsList->robotCopy(robot, robot.name());
-  robotsList->robotCopy(ground, ground.name());
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
 
   mc_rtc::Configuration config;
-  // Add required configuration keys if needed
-  config.add("possibleContacts", std::vector<std::string>{"LeftFoot", "RightFoot"});
   config.add("withMoments", false);
-  config.add("computeRegions", false);
   config.add("HrepMode", false);
   config.add("withFriction", true);
 
   // Create simple contact
-  mc_rbdyn::Contact leftFootContact(const_cast<mc_rbdyn::Robots &>(*robotsList.get()), 0, 1, "LeftFoot", "AllGround");
+  // JVRC1 leg: L_HIP_P -> L_HIP_R -> L_HIP_Y -> L_KNEE -> L_ANKLE_R -> L_ANKLE_P
+  // ==> 6 dofs
+  mc_rbdyn::Contact leftFootContact(*robots, 0, 1, "LeftFoot", "AllGround");
 
   // polytope dimension depending on moments option
   config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
@@ -41,6 +36,8 @@ static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
     mc_dynamic_polytopes::ContactPolytopeJob contactJob;
     contactJob.contactRBDyn_ = &leftFootContact;
     contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
 
     // "job" inputs (simple defaults)
     auto & input = contactJob.input();
@@ -61,30 +58,25 @@ static void BM_PolytopeJVRC1_3D_6DOF(benchmark::State & state)
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_PolytopeJVRC1_3D_6DOF)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_PolytopeJVRC1_3D_6DOF)->Unit(benchmark::kMillisecond)->Repetitions(100)->DisplayAggregatesOnly(true);
 
 static void BM_PolytopeJVRC1_6D_6DOF(benchmark::State & state)
 {
   // Setup part (not timed)
-  auto robots = mc_rbdyn::loadRobot(*mc_rbdyn::RobotLoader::get_robot_module("JVRC1"));
-  auto & robot = robots->robot();
-  auto envs = mc_rbdyn::loadRobot(*mc_rbdyn::RobotLoader::get_robot_module("env/ground"));
-  auto & ground = envs->robot();
-
-  mc_rbdyn::RobotsPtr robotsList(mc_rbdyn::Robots::make());
-  robotsList->robotCopy(robot, robot.name());
-  robotsList->robotCopy(ground, ground.name());
+  auto robotModule = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto ground = mc_rbdyn::RobotLoader::get_robot_module("env/ground");
+  auto robots = mc_rbdyn::loadRobots({robotModule, ground});
+  auto & robot = robots->robot(robotModule->name);
 
   mc_rtc::Configuration config;
-  // Add required configuration keys if needed
-  config.add("possibleContacts", std::vector<std::string>{"LeftFoot", "RightFoot"});
   config.add("withMoments", true);
-  config.add("computeRegions", false);
   config.add("HrepMode", false);
   config.add("withFriction", true);
 
   // Create simple contact
-  mc_rbdyn::Contact leftFootContact(const_cast<mc_rbdyn::Robots &>(*robotsList.get()), 0, 1, "LeftFoot", "AllGround");
+  // JVRC1 leg: L_HIP_P -> L_HIP_R -> L_HIP_Y -> L_KNEE -> L_ANKLE_R -> L_ANKLE_P
+  // ==> 6 dofs
+  mc_rbdyn::Contact leftFootContact(*robots, 0, 1, "LeftFoot", "AllGround");
 
   // polytope dimension depending on moments option
   config("withMoments") ? Rn::setDimension(6) : Rn::setDimension(3);
@@ -96,6 +88,8 @@ static void BM_PolytopeJVRC1_6D_6DOF(benchmark::State & state)
     mc_dynamic_polytopes::ContactPolytopeJob contactJob;
     contactJob.contactRBDyn_ = &leftFootContact;
     contactJob.ctl_ = NULL;
+    contactJob.HrepMode_ = config("HrepMode");
+    contactJob.combineWithFriction_ = config("withFriction");
 
     // "job" inputs (simple defaults)
     auto & input = contactJob.input();
@@ -116,4 +110,4 @@ static void BM_PolytopeJVRC1_6D_6DOF(benchmark::State & state)
   }
 }
 
-BENCHMARK(BM_PolytopeJVRC1_6D_6DOF)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_PolytopeJVRC1_6D_6DOF)->Unit(benchmark::kMillisecond)->Repetitions(100)->DisplayAggregatesOnly(true);
